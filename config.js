4352f7126889eca61b6c8d94457497c0c9a77a33
Paul Schoen <paul@connxus.com>
2019-01-23 00:49:08 -0500

    Testing on Heroku

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+  consumer_key:         'ibY8xUPyG7qiLCEqwv8hVtl82',
+  consumer_secret:      'dCJf9AMD3QzdDf0udKyA0EA7KhIhg7j839Eyfn8uqwA66DNVx7',
+  access_token:         '2369630722-Xa3lvT3CcASbf7Tcn0vSabTYE8chSRMvtecu0O2',
+  access_token_secret:  'Ic4Uc4k0hVn8MgT2Yxt7rK19SlZDIxBHBKuibOZCc5Bgm'
+}
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,73 @@
+const Twit = require('twit'),
+  scryfall = require('scryfall-sdk'),
+  https = require('https');
+
+require('dotenv').config()
+
+const T = new Twit({
+  consumer_key: process.env.BOT_CONSUMER_KEY,
+  consumer_secret: process.env.BOT_CONSUMER_SECRET,
+  access_token: process.env.BOT_ACCESS_TOKEN,
+  access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+});
+
+function postRandomCard() {
+  let base64;
+
+  console.log('Finding a Card...');
+
+  scryfall.Cards.random().then((card) => {
+    https.get(card.image_uris.normal, (res) => {
+      let body = "";
+
+      res.setEncoding('binary');
+
+      res.on('data', function (chunk) {
+        if (res.statusCode == 200) body += chunk;
+      });
+
+      res.on('end', () => {
+        base64 = new Buffer(body, 'binary').toString('base64')
+
+        console.log('Converted!');
+        console.log('Uploading an image...');
+
+        T.post('media/upload', {
+          media_data: base64
+        }, function (err, data) {
+          if (err) {
+            console.log('Something went wrong');
+            console.log(err);
+          } else {
+            console.log('Image uploaded');
+            console.log('Tweeting Now...');
+
+            T.post('statuses/update', {
+                media_ids: new Array(data.media_id_string),
+                status: card.name
+              },
+              function (err) {
+                if (err) {
+                  console.log('Something went wrong...');
+                  console.log(err);
+                } else {
+                  console.log('Posted an image!');
+                }
+              }
+            );
+          }
+        });
+      });
+    }).on('error', (err) => {
+      console.log(`Got error: ${err.message}`);
+    });
+  }).catch();
+};
+
+function callEveryHour() {
+  setInterval(postRandomCard, 1000 * 60 * 60);
+}
+
+postRandomCard();
+
+callEveryHour();
