cae904d96418592df9383fa7cbd8df6f5506c674
Paul Schoen <paul@connxus.com>
2019-01-23 02:24:18 -0500

    Disabled twitter posting and added flavor text and artist to tumblr tag

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const Twit = require('twit'),
 
 require('dotenv').config()
 
-const T = new Twit({
-  consumer_key: process.env.BOT_CONSUMER_KEY,
-  consumer_secret: process.env.BOT_CONSUMER_SECRET,
-  access_token: process.env.BOT_ACCESS_TOKEN,
-  access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
-});
+// const T = new Twit({
+//   consumer_key: process.env.BOT_CONSUMER_KEY,
+//   consumer_secret: process.env.BOT_CONSUMER_SECRET,
+//   access_token: process.env.BOT_ACCESS_TOKEN,
+//   access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+// });
 
 const Tumbs = tumblr.createClient({
   consumer_key: process.env.TUMBLR_CONSUMER_KEY,
@@ -35,44 +35,55 @@ function postRandomCard() {
       });
 
       res.on('end', () => {
-        base64 = new Buffer(body, 'binary').toString('base64')
+        base64 = new Buffer(body, 'binary').toString('base64');
+
+        let caption = `<h1>${card.name}</h1>`;
+
+        if (card.flavor_text) {
+          caption += `<p><i>${card.flavor_text}</i></p>`;
+        }
+
+        if (card.artist) {
+          caption += `<b>Artist: ${card.artist}</b>`;
+        }
 
         console.log('Converted!');
         console.log('Uploading an image...');
+        
 
         Tumbs.createPhotoPost('mtg-cards-hourly.tumblr.com', {
           data64: base64,
-          caption: `<h1>${card.name}</h1><i>Art By: ${card.artist}</i>`,
-          tags: 'mtg, magic the gathering, tcg'
+          caption: caption,
+          tags: `mtg, magic the gathering, tcg, ${card.artist}`
         }, (res) => {
           console.log('Posted On Tumblr')
         });
 
-        T.post('media/upload', {
-          media_data: base64
-        }, function (err, data) {
-          if (err) {
-            console.log('Something went wrong');
-            console.log(err);
-          } else {
-            console.log('Image uploaded');
-            console.log('Tweeting Now...');
+        // T.post('media/upload', {
+        //   media_data: base64
+        // }, function (err, data) {
+        //   if (err) {
+        //     console.log('Something went wrong');
+        //     console.log(err);
+        //   } else {
+        //     console.log('Image uploaded');
+        //     console.log('Tweeting Now...');
 
-            T.post('statuses/update', {
-                media_ids: new Array(data.media_id_string),
-                status: `${card.name} #mtg`
-              },
-              function (err) {
-                if (err) {
-                  console.log('Something went wrong...');
-                  console.log(err);
-                } else {
-                  console.log('Posted an image!');
-                }
-              }
-            );
-          }
-        });
+        //     T.post('statuses/update', {
+        //         media_ids: new Array(data.media_id_string),
+        //         status: `${card.name} #mtg`
+        //       },
+        //       function (err) {
+        //         if (err) {
+        //           console.log('Something went wrong...');
+        //           console.log(err);
+        //         } else {
+        //           console.log('Posted an image!');
+        //         }
+        //       }
+        //     );
+        //   }
+        // });
       });
     }).on('error', (err) => {
       console.log(`Got error: ${err.message}`);
